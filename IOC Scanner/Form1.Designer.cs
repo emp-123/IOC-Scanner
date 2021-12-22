d3e5e61e9242b460f558a70bae2c3941265de575
emp-123 <92721916+emp-123@users.noreply.github.com>
2021-12-22 15:47:21 +0000

    Add files via upload

---
+++ IOC Scanner/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace IOC_Scanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputBox = new System.Windows.Forms.RichTextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.logFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridViewOutput = new System.Windows.Forms.DataGridView();
+            this.ColumnIOC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbEnhanced = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(13, 13);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(864, 396);
+            this.inputBox.TabIndex = 0;
+            this.inputBox.Text = "";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(13, 415);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(279, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Load...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(299, 415);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(279, 23);
+            this.btnGo.TabIndex = 3;
+            this.btnGo.Text = ">>";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(584, 416);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(279, 23);
+            this.btnExport.TabIndex = 4;
+            this.btnExport.Text = "Export CSV";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // logFileDialog
+            // 
+            this.logFileDialog.FileName = "openFileDialog1";
+            this.logFileDialog.Multiselect = true;
+            // 
+            // dataGridViewOutput
+            // 
+            this.dataGridViewOutput.AllowUserToAddRows = false;
+            this.dataGridViewOutput.AllowUserToDeleteRows = false;
+            this.dataGridViewOutput.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridViewOutput.ColumnHeadersHeight = 34;
+            this.dataGridViewOutput.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnIOC,
+            this.ColumnPath});
+            this.dataGridViewOutput.Location = new System.Drawing.Point(881, 13);
+            this.dataGridViewOutput.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridViewOutput.Name = "dataGridViewOutput";
+            this.dataGridViewOutput.ReadOnly = true;
+            this.dataGridViewOutput.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridViewOutput.RowHeadersVisible = false;
+            this.dataGridViewOutput.RowHeadersWidth = 62;
+            this.dataGridViewOutput.RowTemplate.Height = 33;
+            this.dataGridViewOutput.ShowEditingIcon = false;
+            this.dataGridViewOutput.Size = new System.Drawing.Size(877, 425);
+            this.dataGridViewOutput.TabIndex = 9;
+            // 
+            // ColumnIOC
+            // 
+            this.ColumnIOC.HeaderText = "IOC";
+            this.ColumnIOC.MinimumWidth = 8;
+            this.ColumnIOC.Name = "ColumnIOC";
+            this.ColumnIOC.ReadOnly = true;
+            this.ColumnIOC.Width = 250;
+            // 
+            // ColumnPath
+            // 
+            this.ColumnPath.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCellsExceptHeader;
+            this.ColumnPath.HeaderText = "Path";
+            this.ColumnPath.MinimumWidth = 1000;
+            this.ColumnPath.Name = "ColumnPath";
+            this.ColumnPath.ReadOnly = true;
+            this.ColumnPath.Width = 1000;
+            // 
+            // cbEnhanced
+            // 
+            this.cbEnhanced.AutoSize = true;
+            this.cbEnhanced.Enabled = false;
+            this.cbEnhanced.Location = new System.Drawing.Point(550, 346);
+            this.cbEnhanced.Margin = new System.Windows.Forms.Padding(2);
+            this.cbEnhanced.Name = "cbEnhanced";
+            this.cbEnhanced.Size = new System.Drawing.Size(81, 19);
+            this.cbEnhanced.TabIndex = 10;
+            this.cbEnhanced.Text = "Deep Scan";
+            this.cbEnhanced.UseVisualStyleBackColor = true;
+            this.cbEnhanced.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1768, 451);
+            this.Controls.Add(this.cbEnhanced);
+            this.Controls.Add(this.dataGridViewOutput);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.inputBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IOC Scanner";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox inputBox;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.OpenFileDialog logFileDialog;
+        private System.Windows.Forms.DataGridView dataGridViewOutput;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnIOC;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPath;
+        private System.Windows.Forms.CheckBox cbEnhanced;
+    }
+}
+
---
+++ IOC Scanner/Form1.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+
+namespace IOC_Scanner
+{
+
+
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool AllocConsole();
+
+
+
+        List<string> iocList = new List<string>();
+        List<string> sanitizedIp = new List<string>();
+        public static List<string> sanitizedDomain = new List<string>();
+        List<string> sanitizedHash = new List<string>();
+        Dictionary<string, List<string>> csvData = new Dictionary<string, List<string>>();
+
+        List<string> fileList = new List<string>();
+        public static Dictionary<string, List<string>> logsComplete = new Dictionary<string, List<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AllocConsole();
+        }
+
+
+        public Task<IAsyncResult> loadLogs()
+        {
+            var taskComplete = new TaskCompletionSource<IAsyncResult>();
+
+            DialogResult result = logFileDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                foreach (var file in logFileDialog.FileNames)
+                {
+                    fileList.Add(file);
+                 
+
+                }
+                Console.WriteLine("DEBUG: Loading Complete");
+
+            }
+            return taskComplete.Task;
+        }
+
+        public Task<IAsyncResult> loadIOCFromForm()
+        {
+            var taskComplete = new TaskCompletionSource<IAsyncResult>();
+            iocList.Clear();
+            foreach (var line in inputBox.Text.Split('\n'))
+            {
+                iocList.Add(line);
+            }
+            return taskComplete.Task;
+        }
+
+        public Task<IAsyncResult> sortIOC()
+        {
+            var taskComplete = new TaskCompletionSource<IAsyncResult>();
+
+            sanitizedIp.Clear();
+            sanitizedDomain.Clear();
+            sanitizedHash.Clear();
+
+            foreach (var temp in iocList)
+            {
+                if (Regex.Match(temp, @"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}").Success)
+                {
+                    sanitizedIp.Add(temp);
+
+                }
+                else if (Regex.Match(temp, "(?![\\d.]+)((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\\.(xn--)?([a-z0-9\\._-]{1,61}|[a-z0-9-]{1,30})").Success)
+                {
+                    sanitizedDomain.Add(temp);
+                }
+                else if (Regex.Match(temp, "^(((([a-z,1-9]+)|[0-9,A-Z]+))([^a-z\\.]))*").Success)
+                {
+                    sanitizedHash.Add(temp);
+                }
+            }
+            return taskComplete.Task;
+        }
+
+
+
+        public Task<IAsyncResult> exportCSV()
+        {
+            var taskComplete = new TaskCompletionSource<IAsyncResult>();
+
+
+
+            using (var writer = File.AppendText(@"iocs.csv"))
+            {
+                foreach (var i in csvData.Keys)
+                {
+                    foreach (var listItems in csvData[i])
+                    {
+                        writer.WriteLine("{0},{1},", i, listItems);
+                    }
+                }
+            }
+            return taskComplete.Task;
+
+        }
+
+
+        public static int searchposA = 0;
+        public static int searchposB = 0;
+
+
+
+
+
+        public static int entries = sanitizedDomain.Count - 1;
+
+        StringBuilder build = new StringBuilder();
+
+
+
+
+
+
+        public void enhancedScanImportFiles()
+        {
+            foreach (var file in fileList)
+            {
+                using (StreamReader r = new StreamReader(file))
+                {
+                    string line;
+                    while ((line = r.ReadLine()) != null)
+                    {
+                        if (!logsComplete.ContainsKey(line))
+                        {
+                            logsComplete.Add(line, new List<string>());
+                            logsComplete[line].Add(file);
+                        }
+                        else
+                        {
+                            logsComplete[line].Add(file);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static bool matched = false;
+
+
+        public static bool found = false;
+        public static bool first_run = true;
+        public static string ioc = "";
+
+        public void enDo()
+        {
+            foreach (var entr in sanitizedDomain)
+            {
+                foreach (var line in logsComplete.Keys)
+                {
+                    foreach (char temp in line)
+                    {
+                        if (first_run == true)
+                        {
+
+                            ioc = entr;
+                            first_run = false;
+                        }
+                        var aaa = "";
+                        aaa = build.ToString();
+                        if (found == false && aaa != ioc && first_run == false)
+                        {
+                            build.Clear();
+                            searchposA = 0;
+                        }
+
+
+                        string compTemp = ioc.Substring(searchposA, 1);
+                        string compTemp2 = line.Substring(searchposB, 1);
+                        if (compTemp == compTemp2)
+                        {
+                            found = true;
+                            build.Append(compTemp2);
+
+                            if (searchposA == ioc.Length - 1)
+                            {
+                                searchposA = 0;
+                            }
+                            else
+                            {
+                                searchposA++;
+
+                            }
+                            searchposB++;
+                        }
+                        else
+                        {
+                            found = false;
+                            searchposB++;
+                        }
+
+
+                        if (build.ToString() == ioc)
+                        {
+                            StringBuilder builder = new StringBuilder();
+                            foreach (var temp1 in logsComplete[line])
+                            {
+                                builder.Append(temp1).Append(" ");
+                            }
+
+                            Console.WriteLine("STRING MATCH: " + build.ToString() + " " + builder.ToString());
+
+                            this.dataGridViewOutput.Rows.Add(build.ToString(), builder.ToString());
+                            if (!csvData.ContainsKey(build.ToString()))
+                            {
+                                csvData.Add(build.ToString(), new List<string>());
+                                csvData[build.ToString()].Add(builder.ToString());
+                            }
+                            else
+                            {
+                                csvData[build.ToString()].Add(builder.ToString());
+                            }
+
+                            build.Clear();
+                        }
+                    }
+                    first_run = true;
+                    searchposA = 0;
+                    searchposB = 0;
+                    build.Clear();
+
+
+                }
+            }
+
+
+
+        }
+
+        public void enIp()
+        {
+            foreach (var entr in sanitizedIp)
+            {
+                foreach (var line in logsComplete.Keys)
+                {
+                    foreach (char temp in line)
+                    {
+                        if (first_run == true)
+                        {
+
+                            ioc = entr;
+                            first_run = false;
+                        }
+                        var aaa = "";
+                        aaa = build.ToString();
+                        if (found == false && aaa != ioc && first_run == false)
+                        {
+                            build.Clear();
+                            searchposA = 0;
+                        }
+
+
+                        string compTemp = ioc.Substring(searchposA, 1);
+                        string compTemp2 = line.Substring(searchposB, 1);
+                        if (build.ToString() == ioc)
+                        {
+
+                        }
+                        else
+                        {
+                            if (compTemp == compTemp2)
+                            {
+                                found = true;
+                                build.Append(compTemp2);
+
+                                if (searchposA == ioc.Length - 1)
+                                {
+                                    searchposA = 0;
+                                }
+                                else
+                                {
+                                    searchposA++;
+
+                                }
+                                searchposB++;
+                            }
+                            else
+                            {
+                                found = false;
+                                searchposB++;
+                            }
+
+                            if (build.ToString() == ioc)
+                            {
+                                StringBuilder builder = new StringBuilder();
+                                foreach (var temp1 in logsComplete[line])
+                                {
+                                    builder.Append(temp1).Append(" ");
+                                }
+
+                                Console.WriteLine("STRING MATCH: " + build.ToString() + " " + builder.ToString());
+
+                                this.dataGridViewOutput.Rows.Add(build.ToString(), builder.ToString());
+                                if (!csvData.ContainsKey(build.ToString()))
+                                {
+                                    csvData.Add(build.ToString(), new List<string>());
+                                    csvData[build.ToString()].Add(builder.ToString());
+                                }
+                                else
+                                {
+                                    csvData[build.ToString()].Add(builder.ToString());
+                                }
+
+                                build.Clear();
+                            }
+                        }
+
+                    }
+                    first_run = true;
+                    searchposA = 0;
+                    searchposB = 0;
+                    build.Clear();
+
+
+                }
+            }
+
+
+
+        }
+
+        public void enHa()
+        {
+            foreach (var entr in sanitizedHash)
+            {
+                foreach (var line in logsComplete.Keys)
+                {
+                    foreach (char temp in line)
+                    {
+                        if (first_run == true)
+                        {
+
+                            ioc = entr;
+                            first_run = false;
+                        }
+                        if (found == false && build.ToString() != ioc && first_run == false)
+                        {
+                            build.Clear();
+                            searchposA = 0;
+                        }
+
+
+                        string compTemp = ioc.Substring(searchposA, 1);
+                        string compTemp2 = line.Substring(searchposB, 1);
+                        if (compTemp == compTemp2)
+                        {
+                            found = true;
+                            build.Append(compTemp2);
+                            if (searchposA == ioc.Length - 1)
+                            {
+                                searchposA = 0;
+                            }
+                            else
+                            {
+                                searchposA++;
+
+                            }
+                            searchposB++;
+                        }
+                        else
+                        {
+                            found = false;
+                            searchposB++;
+                        }
+
+                        if (build.ToString() == ioc)
+                        {
+                            StringBuilder builder = new StringBuilder();
+                            foreach (var temp1 in logsComplete[line])
+                            {
+                                builder.Append(temp1).Append(" ");
+                            }
+
+                            Console.WriteLine("STRING MATCH: " + build.ToString() + " " + builder.ToString());
+
+                            this.dataGridViewOutput.Rows.Add(build.ToString(), builder.ToString());
+                            if (!csvData.ContainsKey(build.ToString()))
+                            {
+                                csvData.Add(build.ToString(), new List<string>());
+                                csvData[build.ToString()].Add(builder.ToString());
+                            }
+                            else
+                            {
+                                csvData[build.ToString()].Add(builder.ToString());
+                            }
+
+                            build.Clear();
+                        }
+
+                    }
+                    first_run = true;
+                    searchposA = 0;
+                    searchposB = 0;
+                    build.Clear();
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            loadLogs();
+            enhancedScanImportFiles();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            this.dataGridViewOutput.Rows.Clear();
+            loadIOCFromForm();
+            sortIOC();
+            enIp();
+            enDo();
+            enHa();
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            exportCSV();
+        }
+    }
+}
